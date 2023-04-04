7a8d1c239862cdf6c24019317a567e0625a81199
MsIndira <indiraaua@gmail.com>
2023-04-05 00:22:52 +0600

    upd

--- week11_task1/js/index.js
+++ week11_task1/js/index.js
@@ -4,17 +4,19 @@ let goods3 = document.getElementById("vaseprice").innerHTML;
 let goods4 = document.getElementById("stickersprice").innerHTML;
 
 let sum = +goods1 + +goods2 + +goods3 + +goods4;
-//console.log(e);
+//console.log(sum);
 
 let sumDiscount = document.getElementById("totalprice");
 sumDiscount.textContent = sum;
 
 function discount() {
   sumDiscount.textContent = sum * 0.8;
+  document.getElementById("stickersprice").innerHTML = (goods4 * 0.8).toFixed(
+    1
+  );
+  document.getElementById("notebookprice").innerHTML = goods1 * 0.8;
+  document.getElementById("plushtoyprice").innerHTML = goods2 * 0.8;
+  document.getElementById("vaseprice").innerHTML = goods3 * 0.8;
   const btn = document.getElementById("button");
   btn.setAttribute("disabled", "true");
 }
-
-//button.addEventListener("click", () => {
-//  f.textContent = e * 0.8;
-//});
