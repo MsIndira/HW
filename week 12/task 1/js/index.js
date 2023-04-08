8a821bda4183cd6fe7ac7662fdead32286f94c05
MsIndira <indiraaua@gmail.com>
2023-04-08 16:08:08 +0600

    week12 task1

--- week 12/task 1/js/index.js
+++ week 12/task 1/js/index.js
@@ -1,16 +1,34 @@
 function press() {
   let enterName = document.getElementById("fname").value;
-  //alert(enterName);
+
   let firstLetter = enterName.substr(0, 1);
   let restName = enterName.substr(1);
   let firstLetterUp = firstLetter.toUpperCase();
   let restNameLow = restName.toLowerCase();
-  //alert(firstLetterUp + restNameLow);
 
-  let div = document.querySelector("#outcome__result");
-  let p = document.createElement("p");
-  p.innerHTML = firstLetterUp + restNameLow;
-  div.append(p);
+  let enterAvatar = document.getElementById("avatarlink").value;
+
+  let enterComment = document.getElementById("commentfield").value;
+
+  let checkSpam = enterComment.replace(/viagra|XXX/gim, "***");
+
+  let div1 = document.querySelector("#outcome__result");
+
+  let img = document.createElement("img");
+  img.className = "avatar";
+  img.src = enterAvatar;
+  div1.append(img);
+
+  let p1 = document.createElement("p");
+  p1.className = "fio";
+  p1.innerHTML = firstLetterUp + restNameLow;
+  div1.append(p1);
+
+  let div2 = document.querySelector("#outcome");
+  let p2 = document.createElement("p");
+  p2.className = "checkedComment";
+  p2.innerHTML = checkSpam;
+  div2.append(p2);
 }
 
 //<div>
